a266df09b78734794ef6a33a079c3ce3de37fe5d
maxmcginn <maxmcginn01@gmail.com>
2022-03-25 20:04:12 -0500

    Rework React template to use React Router

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # **Team X-Men Spring 2022**
 ### Open Source Tracking project for Commerce Bank
 ----------------------------------------------------
-**March 24, 2022
+**March 24, 2022**
 - Initial commit with template project
+
+**0.0.1** 
+- Add basic react parts
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/"  exact={true} element={<LoginPage />} />
+        <Route path="/home" exact={true} element={<MainPage />} />
+        <Route path="/login" exact={true} element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
